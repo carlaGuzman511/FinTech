2728a770087ab15997703e7900a6898587f8bee7
carlag <mirandagcmarcela@gmail.com>
2026-05-21 13:10:12 -0400

    adding dockerfile + postam collection for the api

--- FinTech/FinTech.API/Program.cs
+++ FinTech/FinTech.API/Program.cs
@@ -8,6 +8,10 @@ using FinTech.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+builder.WebHost.UseUrls($"http://*:{port}");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
